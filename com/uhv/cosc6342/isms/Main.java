f90f4d5b38c960dc80ef01e96a97969314790920
Vishal <vbeeharry@gmail.com>
2023-02-25 15:45:58 -0600

    First test file.

--- com/uhv/cosc6342/isms/Main.java
+++ com/uhv/cosc6342/isms/Main.java
@@ -1,9 +1,33 @@
-package com.uhv.cosc6342;
+package com.uhv.cosc6342.isms;
 
 /**
- * This is the main entry class.
+ * For the iSMS project.
  */
-public class main(String[] args) {
+public class Main {
 
-    System.out.println("Hello World");
+    private int a;
+    private int b;
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        Main m = new Main();
+        m.init();
+        int s = m.sum();
+        System.out.println("The sum is: " + s);
+    }
+
+    private void init() {
+        a = 10;
+        b = 30;
+    }
+
+    private int sum() {
+        int total = a + b;
+        for (int i = 0; i < total; i++) {
+            System.out.println(total);
+        }
+        
+        return total;
+    }
 }
